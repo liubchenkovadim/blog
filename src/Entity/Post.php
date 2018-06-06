72360303c3797b7638b3862f5149381b114287f5
vad <lubcenkovadim039@gmail.com>
2018-06-06 14:54:45 +0300

    blog commit

---
+++ src/Entity/Post.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $datePublic;
+
+    public function __construct()
+    {
+        $this->title = ' ';
+        $this->text = ' ';
+        $this->datePublic = new \Datetime();
+    }
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(?string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getDatePublic(): ?\DateTimeInterface
+    {
+        return $this->datePublic;
+    }
+
+    public function setDatePublic(?\DateTimeInterface $datePublic): self
+    {
+        $this->datePublic = $datePublic;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->date;
+    }
+
+
+}
